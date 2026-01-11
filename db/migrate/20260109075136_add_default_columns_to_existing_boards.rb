9a9d509c3c0e813e0848b10b688b1da79b8cab11
Doan Truong <doan@teamblueotter.com>
2026-01-11 22:25:52 +0700

    Fix migration to include required color field for columns

--- db/migrate/20260109075136_add_default_columns_to_existing_boards.rb
+++ db/migrate/20260109075136_add_default_columns_to_existing_boards.rb
@@ -9,12 +9,21 @@ class AddDefaultColumnsToExistingBoards < ActiveRecord::Migration[8.2]
 
       next if missing_columns.empty?
 
-      missing_columns.each do |column_name|
-        board.columns.create!(
+      default_color_value = Column::Colored::DEFAULT_COLOR.value
+      
+      columns_to_insert = missing_columns.map do |column_name|
+        {
+          id: SecureRandom.uuid,
           name: column_name,
-          account: board.account
-        )
+          board_id: board.id,
+          account_id: board.account_id,
+          color: default_color_value,
+          created_at: Time.current,
+          updated_at: Time.current
+        }
       end
+      
+      Column.insert_all(columns_to_insert) unless columns_to_insert.empty?
     end
   end
 
